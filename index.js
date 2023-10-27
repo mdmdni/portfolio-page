0432d11d430b09d9e96e6639555fc629012248cd
mdmdni <medim3@bfh.ch>
2023-10-27 17:08:53 +0200

    addition of index.js to solve video problems

---
+++ index.js
@@ -0,0 +1,8 @@
+// Get a reference to the video element
+var video = document.getElementById("myVideo");
+
+// Wait for the page to fully load
+window.addEventListener("load", function() {
+    // Play the video
+    video.play();
+});

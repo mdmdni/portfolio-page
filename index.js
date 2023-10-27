3fa2ef80398102d66bfcda9a323772f68f06fa4a
mdmdni <medim3@bfh.ch>
2023-10-27 17:16:01 +0200

    ...

--- index.js
+++
@@ -1,8 +0,0 @@
-// Get a reference to the video element
-var video = document.getElementById("myVideo");
-
-// Wait for the page to fully load
-window.addEventListener("load", function() {
-    // Play the video
-    video.play();
-});
